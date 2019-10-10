3b7e83e0244d515b6d79477c28cecbcdff46d08e
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-10-10 06:49:39 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+class PostValidator < ActiveModel::Validator
+    def validate(record)
+      unless !record.title || record.title.match(/Won[']t Believe | Top \d | Guess /) != nil
+        record.errors[:title] << 'Need to contain "Won\'t Believe", "Top [number]" or "Guess"!'
+      end
+    end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates_with PostValidator
 end
